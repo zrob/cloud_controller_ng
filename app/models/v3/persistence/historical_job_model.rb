1c701dda3f1b12b4da487b30d090551356e86d5e
Annie Sing <asing@pivotal.io>
2017-06-06 16:25:50 -0700

    Mark state of HistoricalJobs
    - When a delayed job completes successfully, we keep track of that
    status by updating the state of the historical job. We mark
    failed jobs similarly.
    
    [#136154453]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- app/models/v3/persistence/historical_job_model.rb
+++ app/models/v3/persistence/historical_job_model.rb
@@ -1,4 +1,7 @@
 module VCAP::CloudController
   class HistoricalJobModel < Sequel::Model(:historical_jobs)
+    PROCESSING_STATE = 'PROCESSING'.freeze
+    COMPLETE_STATE = 'COMPLETE'.freeze
+    FAILED_STATE = 'FAILED'.freeze
   end
 end
