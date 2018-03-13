c2613a41ab2d500354d0b32d540debb2fdc6dff8
Zach Robinson <zrobinson@pivotal.io>
2018-03-13 16:40:58 -0700

    deploy logic relies on decoupled lrp and process guids

--- app/jobs/diego/deploy.rb
+++ app/jobs/diego/deploy.rb
@@ -3,22 +3,25 @@ module VCAP::CloudController
     def perform
 
       Deployment.where(state: 'DEPLOYING').each do |deployment|
-        app = AppModel.find(guid: deployment.app_guid)
-
-        og_proc = app.processes.select { |p| p.type == 'web' }.first
+        app       = AppModel.find(guid: deployment.app_guid)
+        og_proc   = app.processes.select { |p| p.type == 'web' }.first
         next_proc = app.processes.select { |p| p.type == 'web-deploy' }.first
 
-        next unless bbs_instances_client.lrp_instances(next_proc).all? { |lrp| lrp.state == 'RUNNING' }
+        lrps         = bbs_instances_client.lrp_instances(next_proc)
+        all_running  = lrps.all? { |lrp| lrp.state == 'RUNNING' }
+        counts_match = lrps.count == next_proc.instances
+
+        next unless all_running && counts_match
 
         if next_proc.instances == deployment.instances
           deployment.update(state: 'DEPLOYED')
-          og_proc.update(instances: 0)
 
+          og_proc.update(instances: 0)
           og_proc.delete
+
           next_proc.guid = app.guid
           next_proc.type = 'web'
           next_proc.save(validate: false)
-          CloudController::DependencyLocator.instance.runners.runner_for_process(next_proc).start
         else
           next_proc.instances = next_proc.instances + 1
           next_proc.save
